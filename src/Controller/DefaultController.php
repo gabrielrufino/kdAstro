eabd3430c0cb5cdf95d4daac57a346defb0dd28b
Gabriel Rufino <contato@gabrielrufino.com>
2018-05-30 15:22:00 -0300

    Treating form

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,9 +4,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller {
-    public function index() {
-        return $this->render('index.html.twig');
-    }
+  public function index() {
+    return $this->render('index.html.twig');
+	}
+	
+	public function register(Request $request) {
+		$nome = $request->request->get('nome');
+		$nascimento = $request->request->get('nascimento');
+		$cpf = $request->request->get('cpf');
+		$cep = $request->request->get('cep');
+		$estado = $request->request->get('estado');
+		$cidade = $request->request->get('cidade');
+		$bairro = $request->request->get('bairro');
+		$endereco = $request->request->get('endereco');
+		$numero = $request->request->get('numero');
+		$complemento = $request->request->get('complemento');
+
+		return $this->redirectToRoute('checkout');
+	}
+
+	public function checkout() {
+		return $this->render('checkout.html.twig');
+	}
 }
