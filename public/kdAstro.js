9177d8bf82aae398858b272927a02f5077f7464c
Gabriel Rufino <contato@gabrielrufino.com>
2018-05-30 10:46:41 -0300

    validateCPF function

--- public/kdAstro.js
+++ public/kdAstro.js
@@ -1,9 +1,53 @@
-const cleanAddresses = () => {
-  document.getElementById('estado').value = ''
-  document.getElementById('cidade').value = ''
-  document.getElementById('bairro').value = ''
-  document.getElementById('endereco').value = ''
+/* Validate CPF */
+
+const validateCPF = (CPF) => { // Adaptação de https://gist.github.com/suissa/5fc99521933c386bcb905201bdd48c76
+  CPF = document.getElementById('cpf').value
+
+  const mod11 = (num) => num % 11
+  const NOT = (x) => !x
+  const isEqual = (a) => (b) => b === a
+  const mergeDigits = (num1, num2) => `${num1}${num2}`
+  const getTwoLastDigits = (cpf) => `${cpf[ 9 ]}${cpf[ 10 ]}`
+  const getCpfNumeral = (cpf) => cpf.substr(0, 9).split('')
+
+  const isRepeatingChars = (str) =>
+    str.split('').every((elem) => elem === str[ 0 ])
+
+  const toSumOfProducts = (multiplier) => (result, num, i) =>
+    result + (num * multiplier--)
+
+  const getSumOfProducts = (list, multiplier) =>
+    list.reduce(toSumOfProducts(multiplier), 0)
+
+  const getValidationDigit = (multiplier) => (cpf) =>
+    getDigit(mod11(getSumOfProducts(cpf, multiplier)))
+
+  const getDigit = (num) =>
+    (num > 1)
+      ? 11 - num
+      : 0
+
+  const isRepeatingNumbersCpf = isRepeatingChars
+
+  const isValidCPF = (cpf) => {
+    const CPF = getCpfNumeral(cpf)
+    const firstDigit = getValidationDigit(10)(CPF)
+    const secondDigit = getValidationDigit(11)(CPF.concat(firstDigit))
+
+    return isEqual(getTwoLastDigits(cpf))(mergeDigits(firstDigit, secondDigit))
+  }
+
+  if (!(NOT(isRepeatingNumbersCpf(CPF)) && isValidCPF(CPF))) {
+    document.getElementById('cpf').value = ''
+    document.getElementById('cpf-warning').innerHTML = 'Este CPF não é valido. Tente novamente'
+    return false
+  } else {
+    document.getElementById('cpf-warning').innerHTML = ''
+    return true
+  }
 }
+
+/* Get CEP and fill addresses */
 
 const fillAddresses = () => {
   const cep = document.getElementById('cep')
@@ -22,4 +66,4 @@ const fillAddresses = () => {
   }
 }
 
-console.log(fillAddresses)
+console.log(validateCPF, fillAddresses)
