7092114335b326b0b163abd78b2f89475e647a3e
Gabriel Rufino <contato@gabrielrufino.com>
2018-06-01 19:34:13 -0300

    db config

--- README.md
+++ README.md
@@ -2,15 +2,23 @@
 
 Simples cadastro de Pessoa Física e de outros dados.
 
-## Instale e inicie o kdAstro
+## Início rápido
 
-Certifique-se de que o <strong>git</strong>, <strong>php</strong> e <strong>composer</strong> estão instalados em sua máquina e funcionam corretamente na linha de comando. Se tudo estiver certo, prossiga da seguinte forma em sua linha de comando:
+Certifique-se de que o <strong>git</strong>, <strong>php</strong> (Versão 7) e o <strong>composer</strong> estão instalados em sua máquina e funcionam corretamente na linha de comando. Se tudo estiver certo, prossiga da seguinte forma em sua linha de comando:
 
+### Links para download
+<ul>
+	<li><a href="https://getcomposer.org/download/" target="_blank">composer</a></li>
+	<li><a href="https://git-scm.com/" target="_blank">git</a></li>
+	<li><a href="https://secure.php.net/downloads.php" target="_blank">php</a></li>
+</ul>
+
+<strong>Abra o terminal e faça a mágica acontecer:</strong>
 <pre>
-	git clone https://github.com/gabrielrufino/kdAstro
-	cd kdAstro
-	composer install
-	php -S localhost:3000 -t public
+git clone https://github.com/gabrielrufino/kdAstro
+cd kdAstro
+composer install
+php -S localhost:3000 -t public
 </pre>
 
-Em seu browser, acesse http://localhost:3000
+Você já pode acessar http://localhost:3000 em seu navegador e ver uma prévia do sistema. Mas <strong>precisamos ir mais além</strong>. Para que os cadastros sejam registrados, é necessário configurar o banco de dados.
---
+++ src/Entity/Cadastro.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CadastroRepository")
+ */
+class Cadastro
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nome;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $nascimento;
+
+    /**
+     * @ORM\Column(type="string", length=11)
+     */
+    private $cpf;
+
+    /**
+     * @ORM\Column(type="string", length=8)
+     */
+    private $cep;
+
+    /**
+     * @ORM\Column(type="string", length=2)
+     */
+    private $estado;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $cidade;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $bairro;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $endereco;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $numero;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $complemento;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getNascimento(): ?\DateTimeInterface
+    {
+        return $this->nascimento;
+    }
+
+    public function setNascimento(\DateTimeInterface $nascimento): self
+    {
+        $this->nascimento = $nascimento;
+
+        return $this;
+    }
+
+    public function getCpf(): ?string
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(string $cpf): self
+    {
+        $this->cpf = $cpf;
+
+        return $this;
+    }
+
+    public function getCep(): ?string
+    {
+        return $this->cep;
+    }
+
+    public function setCep(string $cep): self
+    {
+        $this->cep = $cep;
+
+        return $this;
+    }
+
+    public function getEstado(): ?string
+    {
+        return $this->estado;
+    }
+
+    public function setEstado(string $estado): self
+    {
+        $this->estado = $estado;
+
+        return $this;
+    }
+
+    public function getCidade(): ?string
+    {
+        return $this->cidade;
+    }
+
+    public function setCidade(string $cidade): self
+    {
+        $this->cidade = $cidade;
+
+        return $this;
+    }
+
+    public function getBairro(): ?string
+    {
+        return $this->bairro;
+    }
+
+    public function setBairro(string $bairro): self
+    {
+        $this->bairro = $bairro;
+
+        return $this;
+    }
+
+    public function getEndereco(): ?string
+    {
+        return $this->endereco;
+    }
+
+    public function setEndereco(string $endereco): self
+    {
+        $this->endereco = $endereco;
+
+        return $this;
+    }
+
+    public function getNumero(): ?int
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(?int $numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getComplemento(): ?string
+    {
+        return $this->complemento;
+    }
+
+    public function setComplemento(?string $complemento): self
+    {
+        $this->complemento = $complemento;
+
+        return $this;
+    }
+}
