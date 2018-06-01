b434863010532c1027725e16c32fd74a1d5e3e53
Gabriel Rufino <contato@gabrielrufino.com>
2018-06-01 16:52:18 -0300

    Controller, routes and templates of success and error pages complete!

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -20,24 +20,38 @@ class DefaultController extends Controller {
 	 * @Route("/register", name="register")
 	 */
 	public function register(Request $request) {
-		$nome = $request->request->get('nome');
-		$nascimento = $request->request->get('nascimento');
-		$cpf = $request->request->get('cpf');
-		$cep = $request->request->get('cep');
-		$estado = $request->request->get('estado');
-		$cidade = $request->request->get('cidade');
-		$bairro = $request->request->get('bairro');
-		$endereco = $request->request->get('endereco');
-		$numero = $request->request->get('numero');
-		$complemento = $request->request->get('complemento');
+		$data = array(
+			'nome' => $request->request->get('nome'),
+			'nascimento' => $request->request->get('nascimento'),
+			'cpf' => $request->request->get('cpf'),
+			'cep' => $request->request->get('cep'),
+			'cidade' => $request->request->get('cidade'),
+			'estado' => $request->request->get('estado'),
+			'bairro' => $request->request->get('bairro'),
+			'endereco' => $request->request->get('endereco'),
+			'numero' => $request->request->get('numero'),
+			'complemento' => $request->request->get('complemento')
+		);
 
-		return $this->redirectToRoute('checkout');
+		$success = true;
+		if ($success) {
+			return $this->redirectToRoute('success');
+		} else {
+			return $this->redirectToRoute('error');
+		}
 	}
 
 	/**
-	 * @Route("/checkout", name="checkout")
+	 * @Route("/success", name="success")
 	 */
-	public function checkout() {
-		return $this->render('checkout.html.twig');
+	public function success() {
+		return $this->render('success.html.twig');
+	}
+
+	/**
+	 * @Route("/error", name="error")
+	 */
+	public function error() {
+		return $this->render('error.html.twig');
 	}
 }
